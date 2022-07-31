54396e8047bc4520809d06e3813ab94a6024a97e
Bubbly <68120990+BubblyDisc@users.noreply.github.com>
2022-07-31 21:59:32 +0100

    e

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,7 @@ import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div style={{ textAlign: "center", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <title>Coming Soon...</title>
-      <h1 style={{ fontSize: "150px", position: "absolute", marginTop: "870px" }}>👀</h1>
-    </div>
+    <Component {...pageProps} />
   )
 }
 
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+    return (
+        <div style={{ textAlign: "center", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <title>Coming Soon...</title>
+            <h1 style={{ fontSize: "150px", position: "absolute", marginTop: "870px" }}>👀</h1>
+        </div>
+    )
+}
